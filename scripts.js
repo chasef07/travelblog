db362740881f8c1443a82ad6e5336007062ab334
Chase Fagen <fagenchase@gmail.com>
2025-02-01 10:27:06 +0700

    february and jan 31

--- scripts.js
+++ scripts.js
@@ -21,6 +21,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Data for blog posts
     const blogs = [
+        { title: 'February 2025', link: 'february-2025.html' },
         { title: 'January 2025', link: 'january-2025.html' },
         { title: 'December 2024', link: 'december-2024.html' },
         { title: 'November 2024', link: 'november-2024.html' },
